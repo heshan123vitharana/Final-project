2adf05a443218a002c339bd79f4bdb0c85d72cac
AshiniGarusinghe28 <ashinigarusinghe740@gmail.com>
2025-08-23 14:54:04 +0530

    Add comments for App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import MillLayout from './MillComponents/MillLayout.jsx';
 
 function App() {
   return (
+    // Wrap the app in Router to enable routing
     <Router>
+      {/* Define application routes */}
       <Routes>
+        {/* All Mill routes handled inside MillLayout */}
         <Route path="/*" element={<MillLayout />} />
       </Routes>
     </Router>
